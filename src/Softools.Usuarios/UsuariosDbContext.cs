7edeca5cd5db50991980f4374abbe33a39d5d097
edDEVjr <edvaldodossantos737@gmail.com>
2025-05-10 18:12:03 -0300

    feat:Update class UsuariosDbContext

--- src/Softools.Usuarios/UsuariosDbContext.cs
+++ src/Softools.Usuarios/UsuariosDbContext.cs
@@ -4,9 +4,15 @@ namespace Softools.Usuarios;
 
 public class UsuariosDbContext : DbContext
 {
+    public DbSet<Usuario> Usuarios { get; set; }
     public UsuariosDbContext(DbContextOptions<UsuariosDbContext> options)
         : base(options)
     {
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Usuario>().HasIndex(u => u.Id).IsUnique();
+        
     }
     
 }
