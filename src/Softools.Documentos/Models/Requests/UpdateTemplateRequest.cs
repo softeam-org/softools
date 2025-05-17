2cbd40f2652286e5156f4f900c1302214aa05cfd
Thiago Menezes <thiagomvas@gmail.com>
2025-05-17 16:40:13 -0300

    feat: Add endpoint para atualizar template

---
+++ src/Softools.Documentos/Models/Requests/UpdateTemplateRequest.cs
@@ -0,0 +1,8 @@
+namespace Softools.Documentos.Models.Requests;
+
+public class UpdateTemplateRequest
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; } = string.Empty;
+    public string? Descricao { get; set; } = string.Empty;
+}
