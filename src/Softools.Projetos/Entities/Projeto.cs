e7c43147e9d3930ed536e9d2602f3efe49c125c6
edDEVjr <edvaldodossantos737@gmail.com>
2025-06-28 20:39:58 -0300

    feat:Add Entidade Projeto

---
+++ src/Softools.Projetos/Entities/Projeto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Softools.Projetos.Entities;
+
+public class Projeto
+{
+    public Projeto()
+    {
+        Id = new Guid();
+        Status = true;
+    }
+    
+    public Guid Id { get; init; }
+    
+    public string Nome { get; set; } = String.Empty;
+    
+    public string Descricao { get; set; } = String.Empty;
+    public bool Status {get; set;}
+
+    public DateOnlyToStringConverter DataInicio{get; set;}
+
+    public DateOnlyToStringConverter DataFim{get; set;}
+    
+    
+
+
+}
