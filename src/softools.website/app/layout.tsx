78e910be9905993bb6e0c63bd65d8bedb506a591
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-08-13 20:31:28 -0300

    feat: Add auth guard to all pages except login and register

--- src/softools.website/app/layout.tsx
+++ src/softools.website/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AuthGuard from "./components/AuthGuard";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -32,9 +23,11 @@ export default function RootLayout({
 </head>
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthGuard>
+          {children}
+        </AuthGuard>
+
       </body>
     </html>
   );
